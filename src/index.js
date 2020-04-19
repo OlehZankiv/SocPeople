8229c185bdd15de192005eb9eb0ecac358fcb682
Oleh Zankic <firegames5645@gmail.com>
2020-04-19 23:36:24 +0300

    38 lesson

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import * as serviceWorker from "./serviceWorker";
-import state from "./redux/state";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import store from "./redux/state";
 
 let render = () => {
-    ReactDOM.render(<App state={state} />, document.getElementById("root"));
+    ReactDOM.render(<App state={store.getState()} />, document.getElementById("root"));
 };
 
-state.subscribe(render);
+store.subscribe(render);
 render();
 
 // If you want your app to work offline and load faster, you can change
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,134 +1,142 @@
-let state = {
-    profile: {
-        posts: {
-            allPosts: [
-                { id: 1, message: "Hello, It's my first props", likeCount: 6 },
-                { id: 2, message: "It's very funny!", likeCount: 11 },
-            ],
-            postText: "",
+let store = {
+    _state: {
+        profile: {
+            posts: {
+                allPosts: [
+                    {
+                        id: 1,
+                        message: "Hello, It's my first props",
+                        likeCount: 6,
+                    },
+                    { id: 2, message: "It's very funny!", likeCount: 11 },
+                ],
+                postText: "",
 
-            addPost() {
-                if (this.postText) {
-                    let newId = this.allPosts.length + 1;
-                    let newPost = {
-                        id: newId,
-                        message: this.postText,
-                        likeCount: 0,
-                    };
-                    this.allPosts.push(newPost);
-                    this.postText = "";
+                addPost() {
+                    if (this.postText) {
+                        let newId = this.allPosts.length + 1;
+                        let newPost = {
+                            id: newId,
+                            message: this.postText,
+                            likeCount: 0,
+                        };
+                        this.allPosts.push(newPost);
+                        this.postText = "";
 
-                    state.render();
-                }
-            },
-            checkPostText(text) {
-                this.postText = text;
-                state.render();
+                        store._render();
+                    }
+                },
+                checkPostText(text) {
+                    this.postText = text;
+                    store._render();
+                },
             },
         },
-    },
-    dialogs: {
-        dialogs: [
-            {
-                id: 1,
-                name: "Aleh",
-                avatar:
-                    "https://i.pinimg.com/236x/0c/a9/e2/0ca9e28dcb12dc698cfd2beda6d6fa64--youtube.jpg",
-            },
-            {
-                id: 2,
-                name: "Ovechka",
-                avatar:
-                    "https://semantica.in/wp-content/uploads/2018/08/av-427845-1.png",
-            },
-            {
-                id: 3,
-                name: "Yana",
-                avatar:
-                    "https://klike.net/uploads/posts/2019-03/1551511801_1.jpg",
-            },
-            {
-                id: 4,
-                name: "Putana",
-                avatar:
-                    "http://gloria-mur.ru/wp-content/uploads/2017/05/avatar1-1024x640.jpg",
-            },
-        ],
-        messages: {
-            allMessages: [
+        dialogs: {
+            dialogs: [
                 {
                     id: 1,
-                    message: "Hi",
+                    name: "Aleh",
                     avatar:
                         "https://i.pinimg.com/236x/0c/a9/e2/0ca9e28dcb12dc698cfd2beda6d6fa64--youtube.jpg",
-                    author: true,
                 },
                 {
                     id: 2,
-                    message: "How are you?",
+                    name: "Ovechka",
                     avatar:
-                        "https://i.pinimg.com/236x/0c/a9/e2/0ca9e28dcb12dc698cfd2beda6d6fa64--youtube.jpg",
-                    author: true,
+                        "https://semantica.in/wp-content/uploads/2018/08/av-427845-1.png",
                 },
                 {
                     id: 3,
-                    message: "Good, are you?",
+                    name: "Yana",
                     avatar:
                         "https://klike.net/uploads/posts/2019-03/1551511801_1.jpg",
-                    author: false,
+                },
+                {
+                    id: 4,
+                    name: "Putana",
+                    avatar:
+                        "http://gloria-mur.ru/wp-content/uploads/2017/05/avatar1-1024x640.jpg",
                 },
             ],
-            textOfArea: "",
-
-            addMessage() {
-                if (this.textOfArea) {
-                    let idMessage =
-                        this.allMessages.length + 1;
-                    let newMessage = {
-                        id: idMessage,
-                        message: this.textOfArea,
+            messages: {
+                allMessages: [
+                    {
+                        id: 1,
+                        message: "Hi",
                         avatar:
                             "https://i.pinimg.com/236x/0c/a9/e2/0ca9e28dcb12dc698cfd2beda6d6fa64--youtube.jpg",
                         author: true,
-                    };
+                    },
+                    {
+                        id: 2,
+                        message: "How are you?",
+                        avatar:
+                            "https://i.pinimg.com/236x/0c/a9/e2/0ca9e28dcb12dc698cfd2beda6d6fa64--youtube.jpg",
+                        author: true,
+                    },
+                    {
+                        id: 3,
+                        message: "Good, are you?",
+                        avatar:
+                            "https://klike.net/uploads/posts/2019-03/1551511801_1.jpg",
+                        author: false,
+                    },
+                ],
+                textOfArea: "",
 
-                    this.allMessages.push(newMessage);
-                }
-                this.textOfArea = "";
-                state.render();
-            },
+                addMessage() {
+                    if (this.textOfArea) {
+                        let idMessage = this.allMessages.length + 1;
+                        let newMessage = {
+                            id: idMessage,
+                            message: this.textOfArea,
+                            avatar:
+                                "https://i.pinimg.com/236x/0c/a9/e2/0ca9e28dcb12dc698cfd2beda6d6fa64--youtube.jpg",
+                            author: true,
+                        };
 
-            checkOfArea(text) {
-                this.textOfArea = text;
-                state.render(state);
+                        this.allMessages.push(newMessage);
+                    }
+                    this.textOfArea = "";
+                    store._render();
+                },
+
+                checkOfArea(text) {
+                    this.textOfArea = text;
+                    store._render();
+                },
             },
         },
+        friends: [
+            {
+                id: 1,
+                img:
+                    "http://gloria-mur.ru/wp-content/uploads/2017/05/avatar1-1024x640.jpg",
+                name: "Putana",
+            },
+            {
+                id: 2,
+                img:
+                    "https://semantica.in/wp-content/uploads/2018/08/av-427845-1.png",
+                name: "Ovechka",
+            },
+            {
+                id: 3,
+                img: "https://klike.net/uploads/posts/2019-03/1551511801_1.jpg",
+                name: "Yana",
+            },
+        ],
     },
-    friends: [
-        {
-            id: 1,
-            img:
-                "http://gloria-mur.ru/wp-content/uploads/2017/05/avatar1-1024x640.jpg",
-            name: "Putana",
-        },
-        {
-            id: 2,
-            img:
-                "https://semantica.in/wp-content/uploads/2018/08/av-427845-1.png",
-            name: "Ovechka",
-        },
-        {
-            id: 3,
-            img: "https://klike.net/uploads/posts/2019-03/1551511801_1.jpg",
-            name: "Yana",
-        },
-    ],
+    getState() {
+        return this._state;
+    },
 
-    render() {},
+    _render() {},
 
     subscribe(observer) {
-        this.render = observer;
+        this._render = observer;
     },
 };
 
-export default state;
+export default store;
